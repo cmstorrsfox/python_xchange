65a316ef8df82f7270e047a923558ec498ebb439
cmstorrsfox <cmstorrsfox@gmail.com>
2021-05-12 16:06:32 +0100

    embedded graph

--- python_xchange.py
+++ python_xchange.py
@@ -1,3 +1,4 @@
+import matplotlib
 import pandas as pd
 import yfinance as yf
 import warnings 
@@ -7,7 +8,10 @@ from tkinter import filedialog
 import sys
 import os
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import mplfinance as mpf
+
+matplotlib.use("TkAgg")
 
 warnings.simplefilter(action="ignore")
 
@@ -32,7 +36,7 @@ def get_stock_info(*args):
 
     #get ticker data
     ticker_name = stocknamevar.get()
-    ticker_symbol = ticker_name.split()[0]
+    ticker_symbol = ticker_name.split(" ")[0]
     period = periodvar.get()
     interval = intervalvar.get()
     output_folder = outputfoldervar.get()
@@ -83,8 +87,18 @@ def get_stock_info(*args):
 
 
     #mplfinance
-    mpf.plot(df, type="candle", style="yahoo", title=ticker_name+" - Interval = "+interval+" - Period = "+period, ylabel="Price ($)")
-    plt.show()
+    fig, ax = mpf.plot(df, type="candle", style="yahoo", title=ticker_name+" - Interval = "+interval+" - Period = "+period, ylabel="Price ($)", returnfig=True)
+    
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+
+    toolbarFrame = Frame(master=root)
+    toolbarFrame.grid(row=7,column=0)
+    toolbar = NavigationToolbar2Tk(canvas, toolbarFrame)
+
+    toolbar.update()
+
+    canvas.get_tk_widget().grid(column=0, row=6, pady=3)
 
     #reset index
     df.reset_index(level=0, inplace=True)
@@ -98,7 +112,7 @@ def get_stock_info(*args):
     df.to_excel(output_folder+"/"+ticker_name+" - Interval = "+interval+" - Period = "+period+".xlsx")
 
 
-    root.destroy()
+    #root.destroy()
     #end of function
 
 
@@ -106,6 +120,7 @@ def get_stock_info(*args):
 #root
 root = Tk()
 root.title("Stock Overview App")
+root.geometry("800x800")
 
 
 #variables
@@ -116,28 +131,28 @@ outputfoldervar = StringVar()
 
 #elements
 content = ttk.Frame(root)
-stock_label = ttk.Label(content, text="Choose a stock")
+stock_label = ttk.Label(content, text="Choose a stock", anchor="w")
 stock = ttk.Combobox(content, values=ticker_list, textvariable=stocknamevar, width=50)
-period_label = ttk.Label(content, text="Choose a period to review")
+period_label = ttk.Label(content, text="Choose a period to review", anchor="w")
 period = ttk.Combobox(content, values=["1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max"], textvariable=periodvar, width=50)
-interval_label = ttk.Label(content, text="Choose an interval")
+interval_label = ttk.Label(content, text="Choose an interval", anchor="w")
 interval = ttk.Combobox(content, values=["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1d", "5d", "1wk", "1mo", "3mo"], textvariable=intervalvar, width=50)
-browse_label = ttk.Label(content, text="Choose where to save the file")
+browse_label = ttk.Label(content, text="Choose where to save the file", anchor="w")
 browse = ttk.Button(content, text="Browse", command=show_folders)
 submit = ttk.Button(content, text="Submit", command=get_stock_info)
 
 
 #layout
-content.grid(column=0, row=0, pady=5, padx=3)
-stock_label.grid(column=0, row=1, pady=5, padx=3)
-stock.grid(column=1, row=1, pady=5, padx=3)
-period_label.grid(column=0, row=2, pady=5, padx=3)
-period.grid(column=1, row=2, pady=5, padx=3)
-interval_label.grid(column=0, row=3, pady=5, padx=3)
-interval.grid(column=1, row=3, pady=5, padx=3)
-browse_label.grid(column=0, row=4, pady=5, padx=3)
-browse.grid(column=1, row=4, pady=5, padx=3)
-submit.grid(column=0, row=5, columnspan=2, pady=5, padx=3)
+content.grid(column=0, row=0, pady=5, padx=100)
+stock_label.grid(column=0, row=1, pady=5)
+stock.grid(column=1, row=1, pady=5)
+period_label.grid(column=0, row=2, pady=5)
+period.grid(column=1, row=2, pady=5)
+interval_label.grid(column=0, row=3, pady=5)
+interval.grid(column=1, row=3, pady=5)
+browse_label.grid(column=0, row=4, pady=5)
+browse.grid(column=1, row=4, pady=5)
+submit.grid(column=1, row=5, columnspan=2, pady=5)
 
 root.mainloop()
 
