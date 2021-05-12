f7c7125e9929e524681338a9edc4912657655df5
cmstorrsfox <cmstorrsfox@gmail.com>
2021-05-12 20:38:55 +0100

    working on progress bar

--- python_xchange.py
+++ python_xchange.py
@@ -10,6 +10,7 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import mplfinance as mpf
+from yfinance.utils import ProgressBar
 
 matplotlib.use("TkAgg")
 
@@ -29,6 +30,60 @@ ticker_list = ticker_data["full"].tolist()
 def show_folders():
     output_folder_path = filedialog.askdirectory()
     outputfoldervar.set(output_folder_path)
+
+#configure progress bar
+def start_progress_bar():
+    progress = ProgressBar(root, orient=HORIZONTAL, length=100, mode="indeterminate")
+    # Function responsible for the updation
+    # of the progress bar value
+    def bar():
+        import time
+        progress['value'] = 20
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 40
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 50
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 60
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 80
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 100
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 80
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 60
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 50
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 40
+        root.update_idletasks()
+        time.sleep(0.5)
+    
+        progress['value'] = 20
+        root.update_idletasks()
+        time.sleep(0.5)
+        progress['value'] = 0
+    
+    bar()
 
 
 #create df function
